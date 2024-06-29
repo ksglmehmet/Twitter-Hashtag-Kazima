a751f800a7bcba4e7e5f56e61e442d161fa9be94
ksglmehmet <mak.kosoglu@gmail.com>
2024-06-30 01:47:54 +0300

    Görüntülenme - Beğeni - Yorum Sayısı gibi verilerinde kazılması için geliştirmeler yapıldı. Hatalar tespit edildi. Düzeltme yapılacak.

--- TwitterHashtagKazima.py
+++ TwitterHashtagKazima.py
@@ -32,7 +32,7 @@ url = "https://x.com/"
 driver.get(url)
 driver.maximize_window()
 
-wait = WebDriverWait(driver, 20) # Otomatik Bekleme Süreleri İçin. 10 burada maksimum bekleme süresini ifade eder. Eğer 10 saniye ve üstü sürerse TimeoutException hatası döner.
+wait = WebDriverWait(driver, 4) # Otomatik Bekleme Süreleri İçin. 10 burada maksimum bekleme süresini ifade eder. Eğer 10 saniye ve üstü sürerse TimeoutException hatası döner.
 
 GirisYap = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[4]/a/div'))) # GirisYap'daki element gözüktüğü zaman işlem yapacak. Manuel time.sleep belirlemeyeceğim. Otomatik çalışacak.
 GirisYap.click()
@@ -46,19 +46,29 @@ Giris.send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-Telefon = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')))
-Telefon.send_keys(GuvenlikKodu)
-Telefon.send_keys(Keys.ENTER)
+try:
+    # İlk olarak Şifre alanını kontrol et
+    Sif = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')))
+    Sif.send_keys(Sifre)
+    Sif.send_keys(Keys.ENTER)
+except:
+    try:
+        # Eğer Şifre alanı bulunamazsa, Telefon alanını kontrol et
+        Telefon = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')))
+        Telefon.send_keys(GuvenlikKodu)
+        Telefon.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Telefon numarası ile işlem yaptıktan sonra Şifre alanını bekle ve işlem yap
+        Sif = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')))
+        Sif.send_keys(Sifre)
+        Sif.send_keys(Keys.ENTER)
+    except Exception as e:
+        print(f"Elementler bulunamadı: {e}")
 
 time.sleep(2)
 
-Sif = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')))
-Sif.send_keys(Sifre)
-Sif.send_keys(Keys.ENTER)
-
-time.sleep(2)
-
-Aranan = "kpss2024"
+Aranan = "EbuCehilTutuklansın"
 Arama = f"https://x.com/search?q=%23{Aranan}&src=typed_query&f=live"
 driver.get(Arama)
 
@@ -72,7 +82,7 @@ for i in Profil:
     Bilgi.append(i.text)
 
 #Hashtag yapısında, sürekli binlerce tweet geldiği için, kaç tane bilgi istiyorsam ona göre döngü yapacağım. Sınırım o olmalı.
-TweetSayisi = 50
+TweetSayisi = 5
 
 #Scrollbar ile hareket ederken ki yeni gelen verilerin çekilmesi.
 while True:
@@ -91,15 +101,23 @@ print(Bilgi)
 sayac = 0
 # Her tweet, Bilgi içerisinde indeksle saklanıyor. İndekslerin içerisine girerek parçalama yapmam lazım.
 for k in range(0, TweetSayisi):
-    Account = (Bilgi[k].split("\n")[1])
-    When = (Bilgi[k].split("\n")[3])
-    Tweet = (Bilgi[k].split("\n")[4])
+    Account = (Bilgi[k].split("\n")[1]) # Kullanıcı Hesabı
+    When = (Bilgi[k].split("\n")[3]) # Ne zaman tweet atıldı
+    Tweet = (Bilgi[k].split("\n")[4]) # Tweet
     if Bilgi[k].split("\n")[5] == " adlı kişiye yanıt olarak":
         Tweet = (Bilgi[k].split("\n")[6])
+    Visualization = (Bilgi[k].split("\n")[-1]) # Görüntülenme
+    Like = (Bilgi[k].split("\n")[-2]) # Beğeni
+    Retweet = (Bilgi[k].split("\n")[-3]) # Yeniden Gönder
+    Comment = (Bilgi[k].split("\n")[-4]) # Yorum Sayısı
     sayac += 1
-    print(f"{sayac} - {When} - {Account}: {Tweet}")
+    print(f"{sayac} - {When} - Görüntülenme-{Visualization} - Beğeni-{Like} - Retweet Sayısı-{Retweet} - Yorum Sayısı-{Comment} - {Account}: {Tweet}")
+    
+print(type(Visualization))
+print(type(Like))
+print(type(Retweet))
      
-
+# IndexError: list index out of range 107. satır için. Try bloğuna al. Metin Dosyasında hata detayı var.
 
 # Tweet Atanlar Kaç Defa Tweet Attı.
 # Hashtag, Belirli Bir Hesabın Tweetleri Bu Çalışmaları Yapılacak.
